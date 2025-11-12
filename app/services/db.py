cfa8719ee42708992f0871b7e9b91a1b611e3597
paolosy1605 <paolosy1605@gmail.com>
2025-11-13 02:24:31 +0800

    Delete app directory

--- app/services/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = AsyncIOMotorClient(MONGO_URL)
-db = client.foodbridge          # 👈 database name: foodbridge
-routes_col = db.routes          # 👈 collection name: routes
-users_col = db.users
